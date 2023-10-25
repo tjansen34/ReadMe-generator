222f75d5f1bbcdc279874b6336ee531c09aa013c
tess jansen <tjansen34@mckaylas-MBP.attlocal.net>
2023-10-24 21:16:10 -0500

    update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs')
-
+const generateMarkdown = require('./generateMarkdown.js');
 
 function generateMarkdown(data) {
     // Your implementation for generating the README content
